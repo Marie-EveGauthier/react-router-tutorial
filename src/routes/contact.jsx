e7da7b217329a5f8b4e553ae4cb79eef0d2ebf47
Marie-EveGauthier <gauthier.marie.eve@gmail.com>
2024-03-12 13:54:53 -0400

    Start a profile without navigate

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,19 +1,20 @@
-import { Form, useLoaderData } from "react-router-dom";
-import { getContact } from "../contacts";
+import { Form, useLoaderData, useFetcher } from "react-router-dom";
+import { getContact, updateContact } from "../contacts";
 
 export async function loader({ params }) {
   const contact = await getContact(params.contactId);
   return { contact };
 }
 
+export async function action({ request, params }) {
+  let formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
+}
+
 export default function Contact() {
   const { contact } = useLoaderData();
-  // {first: "Your",
-  // last: "Name",
-  // avatar: "https://picsum.photos/g/200/200",
-  // twitter: "your_handle",
-  // notes: "Some notes",
-  // favorite: true }
 
   return (
     <div id="contact">
@@ -65,10 +66,15 @@ export default function Contact() {
 }
 
 function Favorite({ contact }) {
+  const fetcher = useFetcher();
   // yes, this is a `let` for later
   let favorite = contact.favorite;
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get("favorite") === "true";
+  }
+
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
@@ -76,6 +82,6 @@ function Favorite({ contact }) {
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }
